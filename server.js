881b15ae68b2e14180ccde991814614889c58a34
Richard Tzanov <richardtzanov@gmail.com>
2018-06-01 15:26:55 +0100

    created routes and index page for plants

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ejsLayouts = require('express-ejs-layouts');
+
+const routes = require('./config/routes');
 
 const app = express();
 
 const { port, dbURI } = require('./config/environment');
 
 mongoose.connect(dbURI);
+
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+app.use(ejsLayouts);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use(routes);
 
 app.listen(port, () => console.log(`Express started on port: ${port}`));
