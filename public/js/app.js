73a58fbd2b12565defc41315de5362d60743c3ea
Richard Tzanov <richardtzanov@gmail.com>
2018-06-02 13:34:41 +0100

    styled add plant page

--- public/js/app.js
+++ public/js/app.js
@@ -22,12 +22,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  const file = document.getElementById('file');
+  file.onchange = function(){
+    if(file.files.length > 0) {
+      document.getElementById('filename').innerHTML = file.files[0].name;
+    }
+  };
+
 });
-
-
-const file = document.getElementById('file');
-file.onchange = function(){
-  if(file.files.length > 0) {
-    document.getElementById('filename').innerHTML = file.files[0].name;
-  }
-};
